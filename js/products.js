e41b7a25457266e3c866a443862eff1d4cbe1a32
apurvrs <apurvrs16@gmail.com>
2026-07-30 14:38:44 +0530

    Add product listings for various anime merchandise

--- js/products.js
+++ js/products.js
@@ -1,1 +1,122 @@
-
+const products = [
+{
+    id:1,
+    name:"Naruto Premium Hoodie",
+    category:"Hoodies",
+    anime:"Naruto",
+    price:1999,
+    image:"images/products/naruto-hoodie.jpg",
+    rating:5,
+    badge:"NEW"
+},
+{
+    id:2,
+    name:"Akatsuki Hoodie",
+    category:"Hoodies",
+    anime:"Naruto",
+    price:2199,
+    image:"images/products/akatsuki-hoodie.jpg",
+    rating:5,
+    badge:"HOT"
+},
+{
+    id:3,
+    name:"Gojo Oversized T-Shirt",
+    category:"T-Shirts",
+    anime:"Jujutsu Kaisen",
+    price:999,
+    image:"images/products/gojo-tshirt.jpg",
+    rating:4.8,
+    badge:"SALE"
+},
+{
+    id:4,
+    name:"Sukuna Hoodie",
+    category:"Hoodies",
+    anime:"Jujutsu Kaisen",
+    price:2499,
+    image:"images/products/sukuna-hoodie.jpg",
+    rating:5,
+    badge:"NEW"
+},
+{
+    id:5,
+    name:"Luffy Hoodie",
+    category:"Hoodies",
+    anime:"One Piece",
+    price:2099,
+    image:"images/products/luffy-hoodie.jpg",
+    rating:5,
+    badge:"HOT"
+},
+{
+    id:6,
+    name:"Zoro T-Shirt",
+    category:"T-Shirts",
+    anime:"One Piece",
+    price:1099,
+    image:"images/products/zoro-shirt.jpg",
+    rating:4.9,
+    badge:"NEW"
+},
+{
+    id:7,
+    name:"One Piece Figure",
+    category:"Figures",
+    anime:"One Piece",
+    price:3499,
+    image:"images/products/luffy-figure.jpg",
+    rating:5,
+    badge:"LIMITED"
+},
+{
+    id:8,
+    name:"Levi Figure",
+    category:"Figures",
+    anime:"Attack on Titan",
+    price:3299,
+    image:"images/products/levi-figure.jpg",
+    rating:5,
+    badge:"HOT"
+},
+{
+    id:9,
+    name:"Mikasa Figure",
+    category:"Figures",
+    anime:"Attack on Titan",
+    price:3399,
+    image:"images/products/mikasa-figure.jpg",
+    rating:4.9,
+    badge:"NEW"
+},
+{
+    id:10,
+    name:"Demon Slayer Poster",
+    category:"Posters",
+    anime:"Demon Slayer",
+    price:499,
+    image:"images/products/demonslayer-poster.jpg",
+    rating:4.8,
+    badge:"SALE"
+},
+{
+    id:11,
+    name:"Tanjiro Hoodie",
+    category:"Hoodies",
+    anime:"Demon Slayer",
+    price:2299,
+    image:"images/products/tanjiro-hoodie.jpg",
+    rating:5,
+    badge:"HOT"
+},
+{
+    id:12,
+    name:"Anime Gaming Headset",
+    category:"Accessories",
+    anime:"Gaming",
+    price:4299,
+    image:"images/products/headset.jpg",
+    rating:5,
+    badge:"BEST"
+}
+];
